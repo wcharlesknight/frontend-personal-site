61bd6b7d5413001e37ec150c5d48d327ca46fe59
wcharlesknight <williamchknight@gmail.com>
2024-04-05 17:28:05 -0700

    changes to pdf

--- src/components/cards/LinkedInResume.tsx
+++ src/components/cards/LinkedInResume.tsx
@@ -37,13 +37,23 @@ const LinkedInResume = () => {
         <>
             <div className="flex flex-col justify-between h-full" style={cardStyle}>
                 <div className="flex flex-row items-center justify-center  w-full h-full border border-white mb-1">
-                    <img onClick={onClick} className="p-2 cursor-pointer" style={{ width: "30%", height: "40%" }} src={LinkedInImage} />
+                    <img
+                        onClick={onClick}
+                        className="p-2 cursor-pointer mr-4"
+                        style={{ width: "30%", height: "40%", boxShadow: "0px 0px 10px 5px rgba(0,0,0,0.2)" }}
+                        src={LinkedInImage}
+                    />
                     <div
                         onMouseEnter={onHoverChangeSize}
                         onMouseLeave={onHoverLeaveChangeSize}
-                        className={`cursor-default ${size === ZOOMED ? "fixed" : ""}`}>
+                        style={{ height: size === ZOOMED ? "500px" : "", overflow: "auto" }}
+                        className={`cursor-default   ${size === ZOOMED ? "fixed" : ""}`}>
                         <Document file={Resume}>
-                            <Page pageNumber={1} height={size} />
+                            {size === ZOOMED ? (
+                                Array.from(new Array(3), (_l, index) => <Page key={`page_${index + 1}`} pageNumber={index + 1} />)
+                            ) : (
+                                <Page pageNumber={1} height={NORMAL} />
+                            )}
                         </Document>
                     </div>
                 </div>
--- src/components/cards/MediumBlog.tsx
+++ src/components/cards/MediumBlog.tsx
@@ -21,7 +21,12 @@ const MediumBlog = () => {
         <>
             <div className="flex flex-col justify-between h-full" style={cardStyle}>
                 <div className="flex flex-row items-center justify-center  w-full h-full border border-white mb-1">
-                    <img onClick={onClick} className="p-2 cursor-pointer" style={{ width: "50%" }} src={MediumImage} />
+                    <img
+                        onClick={onClick}
+                        className="p-2 cursor-pointer "
+                        style={{ width: "50%", boxShadow: "0px 0px 10px 5px rgba(0,0,0,0.2)" }}
+                        src={MediumImage}
+                    />
                 </div>
                 <div className="flex flex-row w-full justify-center items-center mt-1">
                     <BaseCard />
